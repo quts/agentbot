c543f682a8c046535babc43c459257ab2e0fb78d
Vincent Chang <frazda@gmail.com>
2017-03-19 15:46:42 +0800

    add new handler function

---
+++ EvtHdlr.py
@@ -0,0 +1,33 @@
+import linebot.models
+class EventHandler(object):
+	def __init__(self, event):
+		if event.type == 'message':
+			if event.message.type == 'text':
+				return self.TextMessageHandler(event)
+	        if event.message.type == 'sticker': 
+	        	return self.StickerMessageHandler(event)
+		
+		# default handler
+		try:
+		    print event
+		    replyMsg = '%s'%event
+		    line_bot_api.reply_message( event.reply_token, TextSendMessage(text=replyMsg) )
+		except Exception,e:
+			print e
+
+	def TextMessageHandler(self, event):
+        objRating = rating()
+        bResult, replyMsg = objRating.ratingInput(event.message.text)
+        if bResult:
+            line_bot_api.reply_message( event.reply_token, 
+                                        TextSendMessage( text=replyMsg ) )
+
+	def StickerMessageHandler(self, event):
+        if int(event.message.package_id) > 4:
+            print "unable to handle stickers out of list"
+        else:
+            package_id=event.message.package_id
+            sticker_id=event.message.sticker_id
+            line_bot_api.reply_message( event.reply_token, 
+                                        StickerSendMessage( package_id=package_id, 
+                                                            sticker_id=sticker_id ) )
--- app.py
+++ app.py
@@ -9,10 +9,10 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, 
     TextMessage, TextSendMessage, 
-    StickerMessage, StickerSendMessage,
-    TemplateSendMessage, ButtonsTemplate,
 )
+
 from rating import rating
+from EvtHdlr import EventHandler
 
 app = Flask(__name__)
 CHANNEL_ACCESS_TOKEN = os.environ['CHANNEL_ACCESS_TOKEN']
@@ -42,70 +42,9 @@ def callback():
 
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
-def TextMessageHandler(event):
-    print event
-    if event.type == 'message':
-        if event.message.type == 'text':
-            objRating = rating()
-            bResult, replyMsg = objRating.ratingInput(event.message.text)
-            if bResult:
-                line_bot_api.reply_message( event.reply_token, 
-                                            TextSendMessage( text=replyMsg ) )
-
-@handler.add(MessageEvent, message=StickerMessage)
-def StickerMessageHandler(event):
-    print event
-    if event.type == 'message':
-        if event.message.type == 'sticker': 
-            if int(event.message.package_id) > 4:
-                print "unable to handle stickers out of list"
-            else:
-                package_id=event.message.package_id
-                sticker_id=event.message.sticker_id
-                line_bot_api.reply_message( event.reply_token, 
-                                            StickerSendMessage( package_id=package_id, 
-                                                                sticker_id=sticker_id ) )
-
-def LeaveEventHandler(event):
-    #{"source": {"groupId": "C3fc91cd1bae9c8988d05d24b4dfa04a6", "type": "group"}, "timestamp": 1489908217481, "type": "leave"}
-def JoinEventHandler(event):
-    #{"replyToken": "2c110ba76aaf4961832c2cc53cc86da2", "source": {"groupId": "C3fc91cd1bae9c8988d05d24b4dfa04a6", "type": "group"}, "timestamp": 1489908317510, "type": "join"}
-    objTemplate = {
-                      "type": "template",
-                      "altText": "this is a buttons template",
-                      "template": {
-                          "type": "buttons",
-                          "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-                          "title": "Menu",
-                          "text": "Please select",
-                          "actions": [
-                              {
-                                "type": "postback",
-                                "label": "Buy",
-                                "data": "action=buy&itemid=123"
-                              },
-                              {
-                                "type": "postback",
-                                "label": "Add to cart",
-                                "data": "action=add&itemid=123"
-                              },
-                              {
-                                "type": "uri",
-                                "label": "View detail",
-                                "uri": "http://example.com/page/123"
-                              }
-                          ]
-                      }
-                    }
-    line_bot_api.reply_message( event.reply_token, TemplateSendMessage(objTemplate) )
-
-
 @handler.default()
-def default(event):
-    print event
-    replyMsg = '%s'%event
-    line_bot_api.reply_message( event.reply_token, TextSendMessage(text=replyMsg) )
+def defaultHandler(event):
+    objEvent = EventHandler(event)
 
 if __name__ == '__main__':
     app.run(debug=True)
